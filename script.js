c0abd4dd8bde18194758b30288905ce9318c938f
book1001 <61380938+book1001@users.noreply.github.com>
2021-02-22 17:23:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-// Masonry: https://masonry.desandro.com
-var $grid = $('.thumbnail-view').masonry({
-  // options...   isFitWidth: true
-  percentPosition: true,
-  columnWidth: '.thumbnail-view-sizer',
-  // columnWidth: 0.01,
-  itemSelector: '.thumbnail-view-item',
-  horizontalOrder: true
+// // Masonry: https://masonry.desandro.com
+// var $grid = $('.thumbnail-view').masonry({
+//   // options...   isFitWidth: true
+//   percentPosition: true,
+//   columnWidth: '.thumbnail-view-sizer',
+//   // columnWidth: 0.01,
+//   itemSelector: '.thumbnail-view-item',
+//   horizontalOrder: true
+// });
+// // layout Masonry after each image loads
+// $grid.imagesLoaded().progress( function() {
+//   $grid.masonry();
+// });
+
+
+// init Masonry after all images have loaded
+var $grid = $('.thumbnail-view').imagesLoaded( function() {
+  $grid.masonry({
+    percentPosition: true,
+    columnWidth: '.thumbnail-view-sizer',
+    // columnWidth: 0.01,
+    itemSelector: '.thumbnail-view-item',
+    horizontalOrder: true
+  });
 });
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry();
-});
+
 
 // About
 $(document).ready(function(){
