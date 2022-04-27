47169993eb944fb3a7f835d3e1ec1344ea3b9e1e
Halim <61380938+book1001@users.noreply.github.com>
2022-04-27 03:19:52 -0400

    Add files via upload

--- script_maps_streetview.js
+++ script_maps_streetview.js
@@ -1,17 +1,15 @@
-// 스트릿뷰
+
+
 let panorama;
 
-// 지오코딩
-let map;
-let marker;
-let geocoder;
-let responseDiv;
-let response;
-
-// 스트릿뷰
 function initialize() {
+
+
+
+
+
   panorama = new google.maps.StreetViewPanorama(
-    document.getElementById("map"),
+    document.getElementById("street-view"),
     {
       position: { lat: 37.86926, lng: -122.254811 },
       pov: { heading: 165, pitch: 0 },
@@ -20,83 +18,9 @@ function initialize() {
     }
   );
 
-  
-// 지오코딩
-  geocoder = new google.maps.Geocoder();
 
-  const inputText = document.createElement("input");
 
-  inputText.type = "text";
-  inputText.placeholder = "Enter a location";
-
-  const submitButton = document.createElement("input");
-
-  submitButton.type = "button";
-  submitButton.value = "Geocode";
-  submitButton.classList.add("button", "button-primary");
-
-  const clearButton = document.createElement("input");
-
-  clearButton.type = "button";
-  clearButton.value = "Clear";
-  clearButton.classList.add("button", "button-secondary");
-  response = document.createElement("pre");
-  response.id = "response";
-  response.innerText = "";
-  responseDiv = document.createElement("div");
-  responseDiv.id = "response-container";
-  responseDiv.appendChild(response);
-
-  const instructionsElement = document.createElement("p");
-
-  instructionsElement.id = "instructions";
-  instructionsElement.innerHTML =
-    "<strong>Instructions</strong>: Enter an address in the textbox to geocode or click on the map to reverse geocode.";
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
-  map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
-  map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
-  marker = new google.maps.Marker({
-    map,
-  });
-  map.addListener("click", (e) => {
-    geocode({ location: e.latLng });
-  });
-  submitButton.addEventListener("click", () =>
-    geocode({ address: inputText.value })
-  );
-  clearButton.addEventListener("click", () => {
-    clear();
-  });
-  clear();
 }
 
-function clear() {
-  marker.setMap(null);
-  responseDiv.style.display = "none";
-}
-
-function geocode(request) {
-  clear();
-  geocoder
-    .geocode(request)
-    .then((result) => {
-      const { results } = result;
-
-      map.setCenter(results[0].geometry.location);
-      marker.setPosition(results[0].geometry.location);
-      marker.setMap(map);
-      responseDiv.style.display = "block";
-      response.innerText = JSON.stringify(result, null, 2);
-      return results;
-    })
-    .catch((e) => {
-      alert("Geocode was not successful for the following reason: " + e);
-    });
-
-  
-// 스트릿뷰
-}
 
 window.initialize = initialize;
