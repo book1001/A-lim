3d0825ac141a5de8989fb46ec84813bb6d7c76cc
Halim <book1001.db@gmail.com>
2023-07-27 18:35:43 -0400

    Update: FrozenIsland.Live

--- note.js
+++ note.js
@@ -37,3 +37,28 @@ function buttonOnclickTablet() {
 function buttonOnclickPC() {
   // PC: 801px ~에서 동작
 }
+
+
+
+//////////////////////////////////////////////////////
+// 스크린샷 저장: html
+// <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+// <script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
+
+// <button type="button" class="btn_download" style="z-index: 8;">이미지 저장하기</button>
+
+
+// 스크린샷 저장: script
+$(function(){
+    $(".btn_download").click(function(e){
+        html2canvas(document.getElementById("street-view")).then(function(canvas) {
+        // html2canvas(document.getElementsByClassName("gm-style")[0]).then(function(canvas) {
+            var el = document.createElement("a")
+            el.href = canvas.toDataURL("image/jpeg")
+            el.download = '이미지.jpg' //다운로드 할 파일명 설정
+            el.click()
+        })
+    })
+})
